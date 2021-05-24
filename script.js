94a756512629d2841fcf36d6fbf22cc012d2ee67
Raza Ul Kareem <83599698+rez4president@users.noreply.github.com>
2021-05-24 09:50:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+var array = ["Banana", "Apples", "Oranges", "Blueberries"];
+
+
+
+var list = [
+	{
+		username: "Andy",
+		password: "Secret",
+		email: "!23@gmd"
+	},
+
+	{
+		username: "Zert",
+		password: "seerr",
+		email: "1233@gns"
+	},
+] 
+
